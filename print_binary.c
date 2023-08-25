485562ba129d14d52d2305cb6c4ebb4687495c50
Anouar AABOUB <anouar.aaboub@lseg.com>
2023-08-25 14:00:27 +0200

    fix

--- print_binary.c
+++ print_binary.c
@@ -5,12 +5,25 @@
  * @i: param
  * @sum: param
  */
+void do_print_binary(int i, int *sum)
+{
+    if (i != 0)
+        print_binary(i / 2, sum);
+    print_char((i % 2) + '0');
+    (*sum)++;
+}
+
+
+/**
+ * print_binary - print binary
+ * @i: param
+ * @sum: param
+ */
 void print_binary(int i, int *sum)
 {
-	if (i != 0)
-	{
-		print_binary(i / 2, sum);
-		print_char((i % 2) + '0');
-		(*sum)++;
-	}
+    if (i < 0)
+    {
+        print_char(1 + '0');
+    }
+    do_print_binary(abs(i), sum);
 }
